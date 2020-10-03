07ba3aea8ad3fc14d3f2c36fac57cc36cd16cf45
Peer David <david.peer.1991@gmail.com>
2020-10-03 09:32:34 +0200

    Report std and mean

--- scripts/run_sign_experiment.sh
+++ scripts/run_sign_experiment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 
-echo "###################################################"
-echo "EM WITHOUT BIAS"
-python3 train_sign.py --routing="em"
+# # echo "###################################################"
+# echo "EM WITHOUT BIAS"
+# python3 train_sign.py --routing="em"
 
 echo "###################################################"
 echo "RBA WITHOUT BIAS"
 python3 train_sign.py --routing="rba"
 
-echo "###################################################"
-echo "RBA WITH BIAS"
-python3 train_sign.py --routing="rba" --use_bias=True
+# echo "###################################################"
+# echo "RBA WITH BIAS"
+# python3 train_sign.py --routing="rba" --use_bias=True
 
-echo "###################################################"
-echo "EM WITH BIAS"
-python3 train_sign.py --routing="em" --use_bias=True
+# echo "###################################################"
+# echo "EM WITH BIAS"
+# python3 train_sign.py --routing="em" --use_bias=True
--- train_sign.py
+++ train_sign.py
@@ -30,8 +30,8 @@ from data.sign import create_sign_data
 #
 # Learning hyperparameters
 argparser = argparse.ArgumentParser(description="Show limitations of capsule networks")
-argparser.add_argument("--batch_size", default=256, type=int, 
-  help="Learning rate of adam")
+argparser.add_argument("--batch_size", default=64, type=int, 
+  help="Batch size")
 argparser.add_argument("--epochs", default=10, type=int, 
   help="Defines the number of epochs to train the network") 
 argparser.add_argument("--enable_tf_function", default=True, type=bool, 
@@ -40,9 +40,11 @@ argparser.add_argument("--use_bias", default=False, type=bool,
   help="Add a bias term to the preactivation")
 argparser.add_argument("--logging", default=False, type=bool, 
   help="Detailed logging")
+argparser.add_argument("--learning_rate", default=0.0001, type=float, 
+  help="Learning rate of adam")
 
 # Routing properties
-argparser.add_argument("--routing", default="em",
+argparser.add_argument("--routing", default="rba",
   help="rba, em")
 
 # Dataset properties
@@ -58,24 +60,21 @@ def compute_loss(logits, y):
   """ 
   # Calculate margin loss
   loss = margin_loss(logits, tf.one_hot(y, 2), down_weighting=1.0)
-  loss = tf.reduce_mean(loss)
-  loss = loss
-
-  return loss
+  return tf.reduce_mean(loss)
 
 
 def compute_accuracy(logits, labels):
-    predictions = tf.cast(tf.argmax(logits, axis=1), tf.int32)
-    return tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
+  predictions = tf.cast(tf.argmax(tf.nn.softmax(logits), axis=1), tf.int32)
+  return tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
 
 
-def train(train_ds, learning_rate, layers, use_bias):
+def train(train_ds, test_ds, layers, use_bias):
   """ Train capsule networks mirrored on multiple gpu's
   """
 
   # Initialize
   model = SignCapsNet(routing=args.routing, layers=layers, use_bias=use_bias)
-  optimizer = optimizers.Adam(learning_rate=learning_rate)
+  optimizer = optimizers.Adam(learning_rate=args.learning_rate)
   
   # Function for a single training step
   def train_step(inputs):
@@ -87,12 +86,19 @@ def train(train_ds, learning_rate, layers, use_bias):
     grads = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
     acc = compute_accuracy(logits, y)
+
     return loss, acc
   
   def test_step(inputs):
     x, y = inputs
     logits = model(x, y)
     acc = compute_accuracy(logits, y)
+
+    # Ensure correctness
+    tf.debugging.check_numerics(logits, message="Found nan in logits.")
+    tf.debugging.check_numerics(acc, message="Found nan in acc.")
+    tf.debugging.check_numerics(x, message="Found nan in x.")
+
     return acc
 
   if args.enable_tf_function:
@@ -107,17 +113,17 @@ def train(train_ds, learning_rate, layers, use_bias):
     for data in train_ds:
       loss, acc = train_step(data) 
 
-      # Logging
-      if args.logging:
-        print("TRAIN | epoch %d (%d): acc=%.2f, loss=%.6f" % 
-          (epoch, acc, loss), 
-          flush=True)   
+    # Logging
+    if args.logging:
+      print("TRAIN | epoch %d: acc=%.2f, loss=%.6f" % 
+        (epoch, acc, loss), 
+        flush=True)   
 
   ########################################
   # Evaluate accuracy for all datapoints
   ########################################
-  acc = np.mean([test_step(data) for data in train_ds])
-  return acc
+  acc = [test_step(data) for data in test_ds]
+  return np.mean(acc)
 
 
 #
@@ -125,46 +131,39 @@ def train(train_ds, learning_rate, layers, use_bias):
 #
 def main():
 
-  # Load data
-  train_ds = create_sign_data(
-    batch_size = args.batch_size,
-    dataset_size = args.dataset_size)
 
-  ########################################
-  # Architecture & hyperparameter search
-  ########################################
   executions = []
-  total_solved = 0
-  for lr in [0.001, 0.005, 0.01]:
-    for num_hidden_layers in [3,2,1]:
-      for num_caps in [30,25,20,15,10,5]:
-        for caps_dim in [30,25,20,15]:
-          
-          # Using the current architecture we try 3x if we can solve the problem
-          num_retries = 3
-          for i in range(num_retries):
 
-            # Create architecture
-            layers = [(num_caps, caps_dim) for i in range(num_hidden_layers)]
+  for num_hidden_layers in [2,1]:
+    for num_caps in [30,25,20,15]:
+      for caps_dim in [18,16,14,12,10]:
 
-            # Train network
-            acc = train(
-              train_ds, 
-              learning_rate=lr, 
-              layers=layers, 
-              use_bias=args.use_bias)
-
-            # Evaluate solution
-            solved = bool(acc > 0.6)
-            total_solved += int(solved)
-            print("lr=%.5f, num_layers=%d, num_caps=%d, caps_dim=%d | acc=%.3f | solved = %s" % (lr, 
-              num_hidden_layers+1, num_caps, caps_dim, acc, solved), flush=True)
+        num_runs = 3
+        acc_runs = []
+        for run in range(num_runs):
+          # Load data
+          train_ds = create_sign_data(
+            batch_size = args.batch_size,
+            dataset_size = args.dataset_size)
             
-            if solved:
-              break
-          
-          # Add accuracy of solved / not solved execution
-          executions.append(acc)
+          # Create architecture
+          layers = [(num_caps, caps_dim) for _ in range(num_hidden_layers)]
+
+          # Train network
+          acc = train(
+            train_ds, 
+            train_ds,
+            layers=layers, 
+            use_bias=args.use_bias)
+          acc_runs.append(acc)
+
+        # Evaluate solution
+        acc_mean=np.mean(acc_runs)
+        acc_std=np.std(acc_runs)
+        executions.append(acc_mean)
+        solved = bool(acc > 0.6)
+        print("num_layers=%d, num_caps=%d, caps_dim=%d | acc=%.3f(std=%.3f) | solved = %s" %  
+          (num_hidden_layers+1, num_caps, caps_dim, acc_mean, acc_std, solved), flush=True)
   
   #
   # Log results
@@ -172,14 +171,14 @@ def main():
   print("\n==========================", flush=True)
   print("Accuracy | Num solved", flush=True)
   print("==========================", flush=True)
-  for b in [0.5, 0.6, 0.7, 0.8, 0.9]:
+  for b in [0.0, 0.6, 0.7, 0.8, 0.9]:
     num_solved = np.sum([1 if e > b else 0 for e in executions])
     log = "> %.2f | %d" % (b, num_solved)
     print(log, flush=True)
 
     file_name = "experiments/routing_%s_bias_%s.txt" % (args.routing, args.use_bias)
     with open(file_name, 'a') as f:
-      f.write(log)
+      f.write("%s\n" % log)
 
   print("==========================")
 
